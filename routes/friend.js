010d4cb7a96b5de1692f6060c527cb9fdd115975
Kaming Lo <lokaming86@gmail.com>
2025-04-18 16:01:13 +0700

    Udah bisa add friend

--- routes/friend.js
+++ routes/friend.js
@@ -1,12 +1,99 @@
 const express = require("express");
-const bcrypt = require("bcrypt");
 const User = require("../models/user");
-const Score = require("../models/Score");
-
 const router = express.Router();
 
-router.get("/friend", (req, res) =>{
-    res.render("friend");
+// Middleware to check if user is authenticated
+const isAuthenticated = (req, res, next) => {
+    if (req.session && req.session.user) {
+        return next();
+    }
+    res.redirect('/login');
+};
+
+// Get friends page
+router.get("/friend", isAuthenticated, async (req, res) => {
+    try {
+        const user = await User.findById(req.session.user._id)
+            .populate('friends')
+            .populate('friendRequests');
+        
+        const otherUsers = await User.find({
+            _id: { 
+                $nin: [...user.friends, user._id, ...user.friendRequests]
+            }
+        });
+
+        res.render("friend", {
+            user,
+            friends: user.friends,
+            requests: user.friendRequests,
+            otherUsers
+        });
+    } catch (error) {
+        console.error("Error:", error);
+        res.status(500).send("Server error");
+    }
+});
+
+// Send friend request
+router.post("/friend/request", isAuthenticated, async (req, res) => {
+    try {
+        const { friendId } = req.body;
+        await User.findByIdAndUpdate(friendId, {
+            $addToSet: { friendRequests: req.session.user._id }
+        });
+        res.redirect('/friend');
+    } catch (error) {
+        res.status(500).send("Server error");
+    }
+});
+
+// Accept friend request
+router.post("/friend/accept", isAuthenticated, async (req, res) => {
+    try {
+        const { friendId } = req.body;
+        const user = await User.findById(req.session.user._id);
+        const friend = await User.findById(friendId);
+
+        user.friends.push(friendId);
+        friend.friends.push(req.session.user._id);
+        user.friendRequests = user.friendRequests.filter(id => id.toString() !== friendId);
+
+        await user.save();
+        await friend.save();
+        res.redirect('/friend');
+    } catch (error) {
+        res.status(500).send("Server error");
+    }
+});
+
+// Reject friend request
+router.post("/friend/reject", isAuthenticated, async (req, res) => {
+    try {
+        const { friendId } = req.body;
+        await User.findByIdAndUpdate(req.session.user._id, {
+            $pull: { friendRequests: friendId }
+        });
+        res.redirect('/friend');
+    } catch (error) {
+        res.status(500).send("Server error");
+    }
+});
+
+// Remove friend
+router.post("/friend/remove", isAuthenticated, async (req, res) => {
+    try {
+        const { friendId } = req.body;
+        await User.findByIdAndUpdate(req.session.user._id, {
+            $pull: { friends: friendId }
+        });
+        await User.findByIdAndUpdate(friendId, {
+            $pull: { friends: req.session.user._id }
+        });
+        res.redirect('/friend');
+    } catch (error) {
+        res.status(500).send("Server error");
+    }
 });
 
 module.exports = router;
